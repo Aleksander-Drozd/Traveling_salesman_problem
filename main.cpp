6dd32aa3efcf62356a34b89e7e066a4f006ac236
John <aleksander.drozd@outlook.com>
2016-10-12 13:45:42 +0200

    Menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void showMenu() {
+    int input;
+
+    cout << "1) Wczytaj z pliku" << endl;
+    cout << "2) Generuj losowo" << endl;
+    cout << "3) Algorytm podzialu" << endl;
+    cout << "3) Algorytm ograniczen" << endl;
+    cout << "5) Wyjdz" << endl<<endl;
+
+    cin >> input;
+
+    switch (input){
+        case 1:{
+
+        }
+        case 2:{
+
+        }
+        case 3:{
+
+        }
+        case 4:{
+
+        }
+        case 5:{
+            return;
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    showMenu();
     return 0;
 }
